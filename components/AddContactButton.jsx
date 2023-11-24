a15742a6cb594aa84e28f17a51bda6bb97889363
yeyouchuan <yeyouchuan@gamil.com>
2023-11-24 02:46:18 +0000

    input success

--- components/AddContactButton.jsx
+++ components/AddContactButton.jsx
@@ -1,12 +1,22 @@
 'use client';
 
 import React, { useState } from 'react';
+import { Toaster, toast } from 'sonner'
 
 const AddContactAndSendEmail = () => {
   const [email, setEmail] = useState('');
 
+  const promise = () => new Promise((resolve) => setTimeout(resolve, 2000));
+
   const handleSubmit = async () => {
     try {
+      toast.promise(promise, {
+        loading: 'Loading...',
+        success: () => {
+          return `订阅成功！`;
+        },
+      });
+      
       // 添加联系人
       const addContactResponse = await fetch('/api/addContact', {
         method: 'POST',
@@ -14,39 +24,34 @@ const AddContactAndSendEmail = () => {
         body: JSON.stringify({ email }),
       });
 
-      if (!addContactResponse.ok) {
-        throw new Error('Failed to add contact');
-      }
-
       // 发送欢迎邮件
       const sendEmailResponse = await fetch('/api/send', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email }),
       });
-
-      if (!sendEmailResponse.ok) {
-        throw new Error('Failed to send email');
-      }
-
-      alert('Contact added and welcome email sent successfully');
+      
+      setEmail('');
     } catch (error) {
-      alert(error.message);
     }
   };
 
   return (
-    <div>
-      <input
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder="Enter email"
-        className='text-black bg-white rounded-md border-gray-600 border'
-      />
-      <button onClick={handleSubmit} className='text-white bg-gray-800 rounded-md border-gray-600 border'>
-        Subscribe
-      </button>
+    <div className='px-10 py-6 border border-[#262626] bg-[#141414]/50 rounded-lg flex flex-col'>
+      <h2 className='text-white text-2xl font-bold mb-4'>订阅不着陆</h2>
+      <div className='flex gap-1'>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Enter email"
+          className='text-gray-300 bg-[#262626] rounded-md border-gray-600 border py-2 px-4'
+        />
+        <button onClick={handleSubmit} className='text-white bg-[#262626] rounded-md border-gray-600 border py-2 px-4'>
+          Subscribe
+        </button>
+      </div>
+      <Toaster richColors theme='dark'  />
     </div>
   );
 };
