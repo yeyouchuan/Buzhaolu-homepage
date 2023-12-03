bcd13b98aa94454d5c52ef97c18df6d5ed0dc101
yeyouchuan <yeyouchuan@gamil.com>
2023-12-03 11:29:48 +0000

    update contect platform

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,7 @@
+'use client'
+
 import { motion } from 'framer-motion'
+import { useState, useEffect } from 'react'
 
 import { BlogReactions } from '../components/Reactions';
 
@@ -7,8 +10,14 @@ import AddContactButton from '../components/AddContactButton';
 import BackToFuture from '../components/BackToFuture';
 import Cross from '../components/Cross';
 import HomepageDivider from '../components/HomepageDivider';
+import Xiaoyuzhou from '../components/logo/Xiaoyuzhou';
+import Apple from '../components/logo/Apple';
+import Spotify from '../components/logo/Spotify';
 
 export default function Index() {
+
+    const [hoveredText, setHoveredText] = useState({ text: '各种平台', colorClass: 'text-white' });
+    const textClass = `inline-flex mx-1 ${hoveredText.colorClass}`;
 
     return (
         <main className='w-full h-full bg-black flex justify-center selection:bg-gray-50 selection:text-black'>
@@ -32,17 +41,48 @@ export default function Index() {
                     </div>
                 </div>
                 <HomepageDivider />
-                <div className='w-full h-fit grid grid-cols-6 items-center shrink-0 border-b border-[#303030]'>
-                    <div className='col-span-2 h-full border-r border-b border-[#303030]'></div>
-                    <div className='aspect-square border-r border-b border-[#303030]'></div>
-                    <div className='aspect-square border-r border-b border-[#303030]'></div>
-                    <div className='aspect-square border-r border-b border-[#303030]'></div>
+                <div className='w-full h-fit grid grid-cols-4 md:grid-cols-6 items-center shrink-0 border-b border-[#303030]'>
+                    <div className='col-span-4 md:col-span-2 h-full border-r border-b border-[#303030] p-10 md:p-12'>
+                        <div className='flex flex-row md:flex-col text-xl md:text-3xl font-bold gap-0 md:gap-3 justify-center'>
+                            <p className='w-fit'>在
+                                <div className={textClass}>{hoveredText.text}</div>
+                            </p>
+                            <p className='inline-flex'>找到我们</p>
+                        </div>
+                    </div>
+                    <div className='aspect-square border-r border-b border-[#303030]'>
+                        <div 
+                            className='w-full h-full blur-sm hover:blur-none duration-700'
+                            onMouseEnter={() => setHoveredText({ text: '小宇宙', colorClass: 'bg-gradient-to-r bg-clip-text text-transparent from-[#32B3DE] to-[#6BBED3]' })}
+                            onMouseLeave={() => setHoveredText({ text: '各种平台', colorClass: 'text-white' })}
+                        >
+                            <Xiaoyuzhou />
+                        </div>
+                    </div>
+                    <div className='aspect-square border-r border-b border-[#303030]'>
+                        <div 
+                            className='w-full h-full blur-sm hover:blur-none duration-700'
+                            onMouseEnter={() => setHoveredText({ text: 'Apple Podcast', colorClass: 'bg-gradient-to-r bg-clip-text text-transparent from-[#F452FF] to-[#832BC1]' })}
+                            onMouseLeave={() => setHoveredText({ text: '各种平台', colorClass: 'text-white' })}
+                        >
+                            <Apple />
+                        </div>
+                    </div>
+                    <div className='aspect-square border-r border-b border-[#303030]'>
+                        <div 
+                            className='w-full h-full blur-sm hover:blur-none duration-700'
+                            onMouseEnter={() => setHoveredText({ text: 'Spotify', colorClass: 'text-[#9691F9]' })}
+                            onMouseLeave={() => setHoveredText({ text: '各种平台', colorClass: 'text-white' })}
+                        >
+                            <Spotify />
+                        </div>
+                    </div>
                     <div className='aspect-square border-b border-[#303030]'></div>
                     <div className='aspect-square border-r border-[#303030]'></div>
                     <div className='aspect-square border-r border-[#303030]'></div>
                     <div className='aspect-square border-r border-[#303030]'></div>
-                    <div className='aspect-square border-r border-[#303030]'></div>
-                    <div className='aspect-square border-r border-[#303030]'></div>
+                    <div className='aspect-square bottom-0 md:border-r border-[#303030]'></div>
+                    <div className='aspect-square bottom-0 md:border-r border-[#303030] md:w-full md:h-full w-0 h-0'></div>
                     <div className='border-[#303030]'></div>
                 </div>
                 <div className='grid grid-cols-1 md:grid-cols-3 items-center shrink-0 border-b border-[#303030]'>
---
+++ components/logo/Xiaoyuzhou.jsx
@@ -0,0 +1,31 @@
+export default function Xiaoyuzhou() {
+    return (
+        <svg viewBox="0 0 256 256" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g clip-path="url(#clip0_99_29)">
+            <mask id="mask0_99_29" style={{ maskType: 'luminance' }} maskUnits="userSpaceOnUse" x="69" y="70" width="114" height="113">
+            <path d="M69.25 126.6C69.25 157.63 94.59 182.78 125.85 182.78C157.11 182.78 182.45 157.63 182.45 126.6C182.45 95.57 157.11 70.42 125.85 70.42C94.59 70.42 69.25 95.57 69.25 126.6Z" fill="white"/>
+            </mask>
+            <g mask="url(#mask0_99_29)">
+            <path d="M182.44 70.42H69.25V182.78H182.44V70.42Z" fill="url(#paint0_linear_99_29)"/>
+            </g>
+            <mask id="mask1_99_29" style={{ maskType: 'luminance' }} maskUnits="userSpaceOnUse" x="-10" y="21" width="569" height="214">
+            <path d="M558.27 21H-10V234.54H558.27V21Z" fill="white"/>
+            </mask>
+            <g mask="url(#mask1_99_29)">
+            <path d="M75.76 152.76C74.54 150.46 73.47 148.08 72.58 145.61C65.43 153.06 60.1 159.57 56.76 164.96C55.16 167.54 50.28 175.41 54.17 180.45L59.89 176.03C59.89 176.03 59.94 176.11 59.95 176.13C59.51 174.85 61.63 167.9 75.76 152.76Z" fill="white"/>
+            <path d="M197.81 69.52C193.83 64.36 184.62 67.32 181.59 68.29C175.18 70.35 166.74 74.33 157.16 79.81C159.35 81.26 161.44 82.86 163.4 84.59C182.58 73.94 190.5 73.26 192.04 73.95C192.11 74.73 191.88 80.0299 177.84 95.4499C166.89 107.48 151.23 121.54 133.75 135.04C116.27 148.54 98.71 160.14 84.3 167.69C64.23 178.22 59.85 175.98 59.81 175.95C59.81 175.95 59.85 175.98 59.89 176.03L54.17 180.45C55.69 182.42 58.05 183.41 61.33 183.41C66.83 183.41 74.9 180.62 85.92 174.99C101.02 167.27 119.57 155.11 138.16 140.76C156.75 126.4 173.2 111.52 184.49 98.86C197.64 84.11 201.86 74.79 197.8 69.52" fill="white"/>
+            </g>
+            </g>
+            <defs>
+            <linearGradient id="paint0_linear_99_29" x1="124.952" y1="-7.90315" x2="124.952" y2="252.763" gradientUnits="userSpaceOnUse">
+            <stop stop-color="#25B4E1"/>
+            <stop offset="0.37" stop-color="#25B4E1"/>
+            <stop offset="1" stop-color="#E5D5BE"/>
+            </linearGradient>
+            <clipPath id="clip0_99_29">
+            <rect width="256" height="256" fill="white"/>
+            </clipPath>
+            </defs>
+        </svg>
+    )
+}
