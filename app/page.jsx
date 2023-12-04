b9f05409ed62c29a722f18a8c1132d06a5ca07d1
yeyouchuan <yeyouchuan@gamil.com>
2023-12-04 13:36:11 +0000

    update the platform's animation

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { motion } from 'framer-motion'
+import { motion, AnimatePresence } from 'framer-motion'
 import { useState, useEffect } from 'react'
 
 import { BlogReactions } from '../components/Reactions';
@@ -18,6 +18,26 @@ export default function Index() {
 
     const [hoveredText, setHoveredText] = useState({ text: '各种平台', colorClass: 'text-white' });
     const textClass = `inline-flex mx-1 ${hoveredText.colorClass}`;
+
+    useEffect(() => {
+        const handleResize = () => {
+          if (window.innerWidth < 768) {
+            setHoveredText({ text: '各种平台', colorClass: 'text-white' });
+          } else {
+          }
+        };
+    
+        // 监听窗口大小变化
+        window.addEventListener('resize', handleResize);
+    
+        // 初始检查
+        handleResize();
+    
+        // 清除监听器
+        return () => {
+          window.removeEventListener('resize', handleResize);
+        };
+      }, []);
 
     return (
         <main className='w-full h-full bg-black flex justify-center selection:bg-gray-50 selection:text-black'>
@@ -44,35 +64,73 @@ export default function Index() {
                 <div className='w-full h-fit grid grid-cols-4 md:grid-cols-6 items-center shrink-0 border-b border-[#303030]'>
                     <div className='col-span-4 md:col-span-2 h-full border-r border-b border-[#303030] p-10 md:p-12'>
                         <div className='flex flex-row md:flex-col text-xl md:text-3xl font-bold gap-0 md:gap-3 justify-center'>
-                            <p className='w-fit'>在
-                                <div className={textClass}>{hoveredText.text}</div>
-                            </p>
+                            <motion.p 
+                                className='w-fit'
+                            >
+                                在
+                                <AnimatePresence mode="wait">
+                                    <motion.div 
+                                        className={textClass}
+                                        key={hoveredText.text}
+                                        initial={{ opacity: 0, y: -8 }}
+                                        animate={{ opacity: 1, y: 0 }}
+                                        exit={{ opacity: 0, y: 6 }}
+                                        transition={{ duration: 0.2 }}
+                                    >
+                                        {hoveredText.text}
+                                    </motion.div>
+                                </AnimatePresence>
+                            </motion.p>
                             <p className='inline-flex'>找到我们</p>
                         </div>
                     </div>
                     <div className='aspect-square border-r border-b border-[#303030]'>
                         <div 
-                            className='w-full h-full blur-sm hover:blur-none duration-700'
-                            onMouseEnter={() => setHoveredText({ text: '小宇宙', colorClass: 'bg-gradient-to-r bg-clip-text text-transparent from-[#32B3DE] to-[#6BBED3]' })}
-                            onMouseLeave={() => setHoveredText({ text: '各种平台', colorClass: 'text-white' })}
+                            className='w-full h-full blur-sm hover:blur-none duration-700 scale-75'
+                            onMouseEnter={() => {
+                                if (window.innerWidth >= 768) {
+                                    setHoveredText({ text: '小宇宙', colorClass: 'bg-gradient-to-r bg-clip-text text-transparent from-[#32B3DE] to-[#6BBED3]' });
+                                }
+                            }}
+                            onMouseLeave={() => {
+                                if (window.innerWidth >= 768) {
+                                    setHoveredText({ text: '各种平台', colorClass: 'text-white' });
+                                }
+                            }}
                         >
                             <Xiaoyuzhou />
                         </div>
                     </div>
                     <div className='aspect-square border-r border-b border-[#303030]'>
                         <div 
-                            className='w-full h-full blur-sm hover:blur-none duration-700'
-                            onMouseEnter={() => setHoveredText({ text: 'Apple Podcast', colorClass: 'bg-gradient-to-r bg-clip-text text-transparent from-[#F452FF] to-[#832BC1]' })}
-                            onMouseLeave={() => setHoveredText({ text: '各种平台', colorClass: 'text-white' })}
+                            className='w-full h-full blur-sm hover:blur-none duration-700 scale-75'
+                            onMouseEnter={() => {
+                                if (window.innerWidth >= 768) {
+                                    setHoveredText({ text: 'Apple Podcast', colorClass: 'bg-gradient-to-r bg-clip-text text-transparent from-[#F452FF] to-[#832BC1]' });
+                                }
+                            }}
+                            onMouseLeave={() => {
+                                if (window.innerWidth >= 768) {
+                                    setHoveredText({ text: '各种平台', colorClass: 'text-white' });
+                                }
+                            }}
                         >
                             <Apple />
                         </div>
                     </div>
                     <div className='aspect-square border-r border-b border-[#303030]'>
                         <div 
-                            className='w-full h-full blur-sm hover:blur-none duration-700'
-                            onMouseEnter={() => setHoveredText({ text: 'Spotify', colorClass: 'text-[#9691F9]' })}
-                            onMouseLeave={() => setHoveredText({ text: '各种平台', colorClass: 'text-white' })}
+                            className='w-full h-full blur-sm hover:blur-none duration-700 scale-75'
+                            onMouseEnter={() => {
+                                if (window.innerWidth >= 768) {
+                                    setHoveredText({ text: 'Spotify', colorClass: 'text-[#9691F9]' });
+                                }
+                            }}
+                            onMouseLeave={() => {
+                                if (window.innerWidth >= 768) {
+                                    setHoveredText({ text: '各种平台', colorClass: 'text-white' });
+                                }
+                            }}
                         >
                             <Spotify />
                         </div>
